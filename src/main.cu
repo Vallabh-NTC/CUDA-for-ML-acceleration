13d011422f904dff0764d66a5dcff3dc2bc377d4
Vallabh-NTC <vallabh.ansingkar@porsche-nardo.com>
2025-08-23 17:19:36 +0200

    compiler doesnt allow same variable name

--- src/main.cu
+++ src/main.cu
@@ -3,11 +3,11 @@
 
 int main() {
     // Dummy sizes
-    const int N=1, C=3, H=32, W=32;
+    const int N=1, C_=3, H=32, W=32;
     const int K=8, R=3, S=3;
     float *input=nullptr, *filter=nullptr, *output=nullptr;
 
-    cnn::conv_forward(input, filter, output, N,C,H,W, K,R,S, 1,1,1,1);
+    cnn::conv_forward(input, filter, output, N,C_,H,W, K,R,S, 1,1,1,1);
 
     const int M=16, L=8, Kdim=8;
     float *A=nullptr, *B=nullptr, *C=nullptr;
